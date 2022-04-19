f53480a3217a6c2270b4a3f5b048a2edbd26a961
Richard <li.chichih@gmail.com>
2022-04-19 21:30:53 +0800

    whatever

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import Admin from "./Admin";
 
 const App = () => {
   return (
@@ -14,11 +15,14 @@ const App = () => {
           </Link>
         </header>
         <Switch>
-          <Route path="/details/:id">
-            <Details />
+          <Route exact path="/admin">
+            <Admin />
           </Route>
           <Route path="/">
             <SearchParams />
+          </Route>
+          <Route path="/details/:id">
+            <Details />
           </Route>
         </Switch>
       </Router>
